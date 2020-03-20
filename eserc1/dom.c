fb16afbdda1dc49f96701884c00c1f80728f8c86
Domenico-Francesco-Menga <61563877+Domenico-Francesco-Menga@users.noreply.github.com>
2020-03-20 09:48:59 +0100

    Update dom.c

--- eserc1/dom.c
+++ eserc1/dom.c
@@ -6,11 +6,26 @@
  */
 
 #include<stdio.h>
+#define MAX 10;
 
-int main()
-{
-	printf("ciao");
+int main(){
+int i=0;
+int array[MAX]={0};
+int count=0;
 
+for(i=0;i<MAX;i++){
+	printf("\nInserire il %d valore:",i+1);
+	scanf("%d",array[i]);
+}
+	i=0;
+	while(i<MAX){
+		printf("valore: %d",array[i]);
+		if(array[i]%2==0){
+		count++;
+		}
+	}
+	printf("\nson stati inserito %d valori pari",count);
+			
 return 0
 
 }
